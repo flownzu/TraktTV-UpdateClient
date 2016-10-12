509e27280a3fed0b56558e57274576e1102498f5
Florian Jäckel <florian.jaeckel@gmx.net>
2016-10-12 20:52:51 +0200

    Added SyncCompletedEventArgs for future use

---
+++ TraktTV UpdateClient/Cache/SyncCompletedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TraktApiSharp.Objects.Get.Ratings;
+using TraktApiSharp.Objects.Get.Shows;
+
+namespace TraktTVUpdateClient
+{
+    public class SyncCompletedEventArgs : EventArgs
+    {
+        public IEnumerable<TraktShow> Shows;
+        public IEnumerable<TraktRatingsItem> Ratings;
+
+        public SyncCompletedEventArgs(IEnumerable<TraktShow> changedShowList, IEnumerable<TraktRatingsItem> changedRatingsList)
+        {
+
+        }
+
+        public SyncCompletedEventArgs()
+        {
+
+        }
+    }
+}
