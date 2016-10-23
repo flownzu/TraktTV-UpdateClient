bf04c17ed5ff512b1abd3c26a4b7821c8ec1fca3
Florian Jäckel <florian.jaeckel@gmx.net>
2016-10-23 16:41:31 +0200

    Added events to VLCConnection

--- TraktTV UpdateClient/VLCConnection/VLCConnection.cs
+++ TraktTV UpdateClient/VLCConnection/VLCConnection.cs
@@ -6,16 +6,30 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TraktTVUpdateClient
 {
-    class VLCConnection
+    public class VLCConnection
     {
         public int port { get; set; }
         public TcpClient client { get; set; }        
-        public StreamWriter writeStream;
+        public StreamWriter writeStream { get; set; }
 
+        public EventHandler ConnectionLost;
+        public EventHandler MediaItemChanged;
+        public EventHandler PlaybackPaused;
+        public EventHandler PlaybackResumed;
+        public EventHandler PlaybackStopped;
+        public EventHandler WatchedPercentReached;
+
+        private string CurrentMediaItem = String.Empty;
+        private string CurrentMediaTitle = String.Empty;
+        private string CurrentMediaState = String.Empty;
+        private uint CurrentMediaLength = 0;
+        private bool CurrentMediaWatchedPercentReached = false;
+        
         public VLCConnection(int _port)
         {
             client = new TcpClient();
@@ -24,6 +38,47 @@ namespace TraktTVUpdateClient
             client.Connect("localhost", port);
             writeStream = new StreamWriter(client.GetStream());
             read();
+        }
+
+        protected virtual void OnConnectionLost()
+        {
+            CurrentMediaItem = String.Empty;
+            CurrentMediaTitle = String.Empty;
+            CurrentMediaState = String.Empty;
+            CurrentMediaLength = 0;
+            CurrentMediaWatchedPercentReached = false;
+            ConnectionLost?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnMediaItemChanged()
+        {
+            CurrentMediaItem = String.Empty;
+            CurrentMediaTitle = String.Empty;
+            CurrentMediaState = String.Empty;
+            CurrentMediaLength = 0;
+            CurrentMediaWatchedPercentReached = false;
+            MediaItemChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnPlaybackPaused()
+        {
+            PlaybackPaused?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnPlaybackResumed()
+        {
+            PlaybackResumed?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnPlaybackStopped()
+        {
+            PlaybackStopped?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnWatchedPercentReached()
+        {
+            CurrentMediaWatchedPercentReached = true;
+            WatchedPercentReached?.Invoke(this, EventArgs.Empty);
         }
 
         public async Task write(String msg)
@@ -50,6 +105,80 @@ namespace TraktTVUpdateClient
                 }
             }
             return String.Empty;
+        }
+
+        public async void ConnectionThread()
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    await write("status");
+                    string statusResponse = await read();
+                    await write("get_time");
+                    string getTimeResponse = await read();
+                    if (CurrentMediaLength == 0)
+                    {
+                        await write("get_length");
+                        string getLengthResponse = await read();
+                        if (!String.IsNullOrEmpty(getLengthResponse.Trim()) && !String.IsNullOrWhiteSpace(getLengthResponse.Trim())) { UInt32.TryParse(getLengthResponse.Trim(), out CurrentMediaLength); }
+                    }
+
+                    Match m = Regex.Match(statusResponse, @"new input\: (.*) \)(?:[\S\s].*){3}state (\w+)");
+                    if (m.Success)
+                    {
+                        string MediaItem = m.Groups[1].Value;
+                        if (!CurrentMediaItem.Equals(MediaItem))
+                        {
+                            Debug.WriteLine("Current Media: " + CurrentMediaItem + Environment.NewLine + "New Media: " + MediaItem);
+                            OnMediaItemChanged();
+                            CurrentMediaItem = MediaItem;
+                            await write("get_title");
+                            string getTitleResponse = await read();
+                            if (!String.IsNullOrEmpty(getTitleResponse)) { CurrentMediaTitle = getTitleResponse; }
+                        }
+                        string MediaState = m.Groups[2].Value;
+                        if (CurrentMediaState.Equals(String.Empty)) { CurrentMediaState = MediaState; }
+                        else
+                        {
+                            if (!CurrentMediaState.Equals(MediaState))
+                            {
+                                CurrentMediaState = MediaState;
+                                if (CurrentMediaState.Equals("playing"))
+                                {
+                                    OnPlaybackResumed();
+                                }
+                                else if (CurrentMediaState.Equals("paused"))
+                                {
+                                    OnPlaybackPaused();
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (statusResponse.Contains("state stopped"))
+                        {
+                            CurrentMediaState = "stopped";
+                            OnPlaybackStopped();
+                        }
+                    }
+
+                    if(!String.IsNullOrWhiteSpace(getTimeResponse) && CurrentMediaLength > 0)
+                    {
+                        uint currentTime = 0;
+                        UInt32.TryParse(getTimeResponse.Trim(), out currentTime);
+                        float watchedPercent = (float)currentTime / (float)CurrentMediaLength;
+                        if(!CurrentMediaWatchedPercentReached && watchedPercent >= Properties.Settings.Default.WatchedPercent)
+                        {
+                            OnWatchedPercentReached();
+                        }
+                    }
+                    Thread.Sleep(1000);
+                }
+                catch (IOException ex) { break; }
+            }
+            OnConnectionLost();
         }
     }
 }
