73a56ab94c607d6ec489588793136ad27685a79b
Florian Jäckel <florian.jaeckel@gmx.net>
2016-10-29 13:44:47 +0200

    Moved Word-Similarity calculation to Extensions

--- TraktTV UpdateClient/Extension/Extensions.cs
+++ TraktTV UpdateClient/Extension/Extensions.cs
@@ -65,5 +65,132 @@ namespace TraktTVUpdateClient.Extension
         }
 
         public static void Forget(this Task t) { }
+
+        public static double GetSimilarityRatio(String FullString1, String FullString2, out double WordsRatio, out double RealWordsRatio)
+        {
+            double theResult = 0;
+            String[] Splitted1 = FullString1.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            String[] Splitted2 = FullString2.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if (Splitted1.Length < Splitted2.Length)
+            {
+                String[] Temp = Splitted2;
+                Splitted2 = Splitted1;
+                Splitted1 = Temp;
+            }
+            int[,] theScores = new int[Splitted1.Length, Splitted2.Length];
+            int[] BestWord = new int[Splitted1.Length];
+
+            for (int loop = 0; loop < Splitted1.Length; loop++)
+            {
+                for (int loop1 = 0; loop1 < Splitted2.Length; loop1++) theScores[loop, loop1] = 1000;
+                BestWord[loop] = -1;
+            }
+            int WordsMatched = 0;
+            for (int loop = 0; loop < Splitted1.Length; loop++)
+            {
+                String String1 = Splitted1[loop];
+                for (int loop1 = 0; loop1 < Splitted2.Length; loop1++)
+                {
+                    String String2 = Splitted2[loop1];
+                    int LevenshteinDistance = Compute(String1, String2);
+                    theScores[loop, loop1] = LevenshteinDistance;
+                    if (BestWord[loop] == -1 || theScores[loop, BestWord[loop]] > LevenshteinDistance) BestWord[loop] = loop1;
+                }
+            }
+
+            for (int loop = 0; loop < Splitted1.Length; loop++)
+            {
+                if (theScores[loop, BestWord[loop]] == 1000) continue;
+                for (int loop1 = loop + 1; loop1 < Splitted1.Length; loop1++)
+                {
+                    if (theScores[loop1, BestWord[loop1]] == 1000) continue;
+                    if (BestWord[loop] == BestWord[loop1])
+                    {
+                        if (theScores[loop, BestWord[loop]] <= theScores[loop1, BestWord[loop1]])
+                        {
+                            theScores[loop1, BestWord[loop1]] = 1000;
+                            int CurrentBest = -1;
+                            int CurrentScore = 1000;
+                            for (int loop2 = 0; loop2 < Splitted2.Length; loop2++)
+                            {
+                                if (CurrentBest == -1 || CurrentScore > theScores[loop1, loop2])
+                                {
+                                    CurrentBest = loop2;
+                                    CurrentScore = theScores[loop1, loop2];
+                                }
+                            }
+                            BestWord[loop1] = CurrentBest;
+                        }
+                        else
+                        {
+                            theScores[loop, BestWord[loop]] = 1000;
+                            int CurrentBest = -1;
+                            int CurrentScore = 1000;
+                            for (int loop2 = 0; loop2 < Splitted2.Length; loop2++)
+                            {
+                                if (CurrentBest == -1 || CurrentScore > theScores[loop, loop2])
+                                {
+                                    CurrentBest = loop2;
+                                    CurrentScore = theScores[loop, loop2];
+                                }
+                            }
+                            BestWord[loop] = CurrentBest;
+                        }
+
+                        loop = -1;
+                        break;
+                    }
+                }
+            }
+            for (int loop = 0; loop < Splitted1.Length; loop++)
+            {
+                if (theScores[loop, BestWord[loop]] == 1000) theResult += Splitted1[loop].Length;
+                else
+                {
+                    theResult += theScores[loop, BestWord[loop]];
+                    if (theScores[loop, BestWord[loop]] == 0) WordsMatched++;
+                }
+            }
+            int theLength = (FullString1.Replace(" ", "").Length > FullString2.Replace(" ", "").Length) ? FullString1.Replace(" ", "").Length : FullString2.Replace(" ", "").Length;
+            if (theResult > theLength) theResult = theLength;
+            theResult = (1 - (theResult / theLength)) * 100;
+            WordsRatio = ((double)WordsMatched / (double)Splitted2.Length) * 100;
+            RealWordsRatio = ((double)WordsMatched / (double)Splitted1.Length) * 100;
+            return theResult;
+        }
+
+        public static int Compute(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            return d[n, m];
+        }
     }
 }
