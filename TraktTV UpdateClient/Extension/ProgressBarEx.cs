36406408fa6b701da5e3a4c92baa2780a6e9270a
Florian Jäckel <florian.jaeckel@gmx.net>
2016-11-18 19:45:18 +0100

    Changed progress bar style

--- TraktTV UpdateClient/Extension/ProgressBarEx.cs
+++ TraktTV UpdateClient/Extension/ProgressBarEx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
@@ -17,29 +19,26 @@ namespace TraktTVUpdateClient.Extension
         public ProgressBarEx()
         {
             if (ProgressBarRenderer.IsSupported) { SetStyle(ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true); }
+            BackColor = Color.DarkTurquoise;
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            if (ProgressBarRenderer.IsSupported)
+            LinearGradientBrush brush = null;
+            Rectangle rec = ClientRectangle;
+            double scaleFactor = ((double)Value - (double)Minimum) / ((double)Maximum - (double)Minimum);
+            ProgressBarRenderer.DrawHorizontalBar(e.Graphics, rec);
+            rec.Width = (int)((rec.Width * scaleFactor) - 4);
+            rec.Height -= 4;
+            if(rec.Width == 0) { rec.Width = -1; }
+            brush = new LinearGradientBrush(rec, this.ForeColor, this.BackColor, LinearGradientMode.Vertical);
+            e.Graphics.FillRectangle(brush, 2, 2, rec.Width, rec.Height);
+            string text = DisplayStyle == ProgressBarDisplayText.Percentage ? Value.ToString() + '%' : CustomText;
+            using (Font f = new Font("Calibri", 10))
             {
-                Rectangle rect = ClientRectangle;
-                Graphics g = e.Graphics;
-                ProgressBarRenderer.DrawHorizontalBar(g, rect);
-                rect.Inflate(-3, -3);
-                if (Value > 0)
-                {
-                    Rectangle clip = new Rectangle(rect.X, rect.Y, (int)Math.Round(((float)Value / Maximum) * rect.Width), rect.Height);
-                    ProgressBarRenderer.DrawHorizontalChunks(g, clip);
-                }
-                string text = DisplayStyle == ProgressBarDisplayText.Percentage ? Value.ToString() + '%' : CustomText;
-                using (Font f = new Font(FontFamily.GenericSerif, 10))
-                {
-                    SizeF len = g.MeasureString(text, f);
-                    // Point location = new Point(Convert.ToInt32((rect.Width / 2) - (len.Width / 2)), Convert.ToInt32((rect.Height / 2) - (len.Height / 2)));
-                    Point location = new Point(Convert.ToInt32((Width / 2) - len.Width / 2), Convert.ToInt32((Height / 2) - len.Height / 2));
-                    g.DrawString(text, f, Brushes.Red, location);
-                }
+                SizeF len = e.Graphics.MeasureString(text, f);
+                Point location = new Point(Convert.ToInt32((Width / 2) - len.Width / 2), Convert.ToInt32((Height / 2) - len.Height / 2));
+                e.Graphics.DrawString(text, f, Brushes.Black, location);
             }
         }
 
