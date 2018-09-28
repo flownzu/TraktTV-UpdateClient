e0546dd1ba7a85639ae9a0615834cf90cc0c92a4
Florian Jäckel <florian.jaeckel@gmx.net>
2018-09-28 16:54:19 +0200

    Forgot to actually add the new files in the commit...woops!

---
+++ TraktTV UpdateClient/Forms/MissingEpisodesForm/MissingEpisodesForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TraktTVUpdateClient.Forms
+{
+    partial class MissingEpisodeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MissingEpisodeForm));
+            this.exportBtn = new System.Windows.Forms.Button();
+            this.treeViewMissingEpisodes = new TraktTVUpdateClient.TreeViewEx();
+            this.checkBoxSeasonExport = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // exportBtn
+            // 
+            this.exportBtn.Location = new System.Drawing.Point(212, 453);
+            this.exportBtn.Name = "exportBtn";
+            this.exportBtn.Size = new System.Drawing.Size(209, 41);
+            this.exportBtn.TabIndex = 2;
+            this.exportBtn.Text = "CSV Export";
+            this.exportBtn.UseVisualStyleBackColor = true;
+            this.exportBtn.Click += new System.EventHandler(this.ExportBtn_Click);
+            // 
+            // treeViewMissingEpisodes
+            // 
+            this.treeViewMissingEpisodes.Location = new System.Drawing.Point(12, 12);
+            this.treeViewMissingEpisodes.Name = "treeViewMissingEpisodes";
+            this.treeViewMissingEpisodes.Size = new System.Drawing.Size(409, 435);
+            this.treeViewMissingEpisodes.TabIndex = 3;
+            // 
+            // checkBoxSeasonExport
+            // 
+            this.checkBoxSeasonExport.AutoSize = true;
+            this.checkBoxSeasonExport.Location = new System.Drawing.Point(12, 466);
+            this.checkBoxSeasonExport.Name = "checkBoxSeasonExport";
+            this.checkBoxSeasonExport.Size = new System.Drawing.Size(194, 17);
+            this.checkBoxSeasonExport.TabIndex = 4;
+            this.checkBoxSeasonExport.Text = "Export whole season as single entry";
+            this.checkBoxSeasonExport.UseVisualStyleBackColor = true;
+            // 
+            // MissingEpisodeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 504);
+            this.Controls.Add(this.checkBoxSeasonExport);
+            this.Controls.Add(this.treeViewMissingEpisodes);
+            this.Controls.Add(this.exportBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MissingEpisodeForm";
+            this.Text = "Missing Episodes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button exportBtn;
+        private TreeViewEx treeViewMissingEpisodes;
+        private System.Windows.Forms.CheckBox checkBoxSeasonExport;
+    }
+}
---
+++ TraktTV UpdateClient/Forms/MissingEpisodesForm/MissingEpisodesForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TraktTVUpdateClient.Cache;
+
+namespace TraktTVUpdateClient.Forms
+{
+    public partial class MissingEpisodeForm : Form
+    {
+        public MissingEpisodeForm(TraktCache cache)
+        {
+            InitializeComponent();
+            foreach (var showProgress in cache.ProgressList.OrderBy(x => x.Key))
+            {
+                if (showProgress.Value.Seasons.Any(x => x.Completed < x.Aired))
+                {
+                    var showTitle = cache.WatchedList.Where(x => x.Show.Ids.Slug == showProgress.Key).FirstOrDefault()?.Show.Title;
+                    if (!string.IsNullOrEmpty(showTitle))
+                    {
+                        var showNode = treeViewMissingEpisodes.Nodes.Add(showTitle);
+                        foreach (var season in showProgress.Value.Seasons)
+                        {
+                            if (season.Episodes.Any(x => !x.Completed ?? false))
+                            {
+                                var seasonNode = showNode.Nodes.Add("Season " + season.Number);
+                                if (season.Episodes.All(x => !x.Completed ?? false)) seasonNode.Tag = "complete";
+                                else seasonNode.Tag = "partial";
+                                foreach (var episode in season.Episodes)
+                                {
+                                    if (!episode.Completed ?? false)
+                                    {
+                                        seasonNode.Nodes.Add("Episode " + episode.Number);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void ExportBtn_Click(object sender, System.EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "csv-file (*.csv)|*.csv",
+                FileName = "MissingEpisodes" + DateTime.Now.ToString("ddMMyyyyhhmmss") + ".csv",
+                RestoreDirectory = true,
+                OverwritePrompt = true,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+            var dialogResult = saveFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                try
+                {
+                    StringBuilder sb = new StringBuilder("SHOW;SEASON;EPISODE" + Environment.NewLine);
+                    foreach (TreeNode showNode in treeViewMissingEpisodes.Nodes)
+                    {
+                        foreach (TreeNode seasonNode in showNode.Nodes)
+                        {
+                            if (checkBoxSeasonExport.Checked && seasonNode.Tag.ToString() == "complete")
+                            {
+                                sb.AppendLine(showNode.Text + ";" + seasonNode.Text.Replace("Season ", "") + ";");
+                            }
+                            else
+                            {
+                                foreach (TreeNode episodeNode in seasonNode.Nodes)
+                                {
+                                    sb.AppendLine(showNode.Text + ";" + seasonNode.Text.Replace("Season ", "") + ";" + episodeNode.Text.Replace("Episode ", ""));
+                                }
+                            }
+                        }
+                    }
+                    File.WriteAllText(saveFileDialog.FileName, sb.ToString());
+                }
+                catch { }
+            }
+        }
+    }
+}
